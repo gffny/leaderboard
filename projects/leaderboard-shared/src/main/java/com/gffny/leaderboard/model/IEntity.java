20d220ee387aa6f57a58e703429278c20d0d7a68
gffny <MPWQ74@motorola.com>
2013-01-16 22:33:09 -0500

    Created superclass for entities with appropriate behaviour in interface

---
+++ projects/leaderboard-shared/src/main/java/com/gffny/leaderboard/model/IEntity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.gffny.leaderboard.model;
+
+/**
+ * @author John Gaffney (john@gffny.com) Jan 14, 2013
+ * 
+ */
+public interface IEntity {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getName();
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int getId();
+}
